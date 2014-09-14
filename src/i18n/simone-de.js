a68d27e839a74a98a079c0a9f83977fc3154cba9
Bernhard Essl <bernhardessl@gmail.com>
2014-09-14 09:33:21 +0200

    Added german i18n file.

---
+++ src/i18n/simone-de.js
@@ -0,0 +1,43 @@
+/*!
+ * Simone - German localization file
+ *
+ * Copyright 2014 Cezary Kluczyński
+ * Version: @simone-main-version
+ * Released under the MIT license.
+ *
+ * http://cezarykluczynski.github.io/simone/docs/documentation.html
+ * http://cezarykluczynski.github.io/simone/docs/nonTechnicalContributions.html#translations
+ */
+;(function ( $ ) {
+var langCode = "de";
+$.simone.taskbar.prototype.options.localization[ langCode ] =
+	$.simone.taskbar.prototype.options.localization[ langCode ] || {};
+$.extend( true, $.simone.taskbar.prototype.options.localization[ langCode ], {
+	alwaysOnTop: "Immer oben",
+	code: langCode,
+	clockDateFormat: "DD, d MM, yy",
+	clockTimeFormat: "HH:mm",
+	clockAmSymbol: "AM",
+	clockPmSymbol: "PM",
+	close: "Schließen",
+	confirmCloseTitle: "Schließen von “:title” bestätigen",
+	confirmCloseNo: "Nein",
+	confirmCloseText: "Wollen sie wirklich das Fenster schließen?",
+	confirmCloseYes: "Ja",
+	languageSelect: "Englisch ist die aktuelle Sprache. Klicken um sie zu ändern",
+	maximize: "Maximieren",
+	minimize: "Minimieren",
+	minimizeAll: "Alle Fenster minimieren",
+	multipleWindowButton: "(:counter) :title",
+	name: "Deutsch",
+	networkMonitorOffline: "Das Gerät scheint nicht mit dem Web verbunden zu sein",
+	networkMonitorOnline: "Das Gerät scheint mit dem Web verbunden zu sein",
+	newWindow: "Neues Fenster",
+	newWindowNext: "Neues Fenster (:counter)",
+	restore: "Wiederherstellen",
+	slug: "DE",
+	"startButton:start": "Start",
+	toggleFullscreenEnter: "Vollbildschirmansicht aktivieren",
+	toggleFullscreenLeave: "Vollbildschirmansicht beenden",
+});
+})( jQuery );
